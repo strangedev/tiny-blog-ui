952315257cbad2bdf1868cfb4284a7581fa34d39
Noah Hummel <n.p.hummel@gmail.com>
2019-03-02 14:16:57 +0100

    [FEATURE] add Collapsible

--- src/components/Collapsible.vue
+++ src/components/Collapsible.vue
@@ -1,10 +1,38 @@
 <template>
-    
+    <div>
+        <p v-on:click="toggle()">
+            {{ preview }}
+        </p>
+        <div v-if="expanded">
+            <slot name="content"></slot>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Collapsible"
+        name: "Collapsible",
+        props: {
+            preview: {
+                type: String,
+                default: "Lorem ipsum sit dolor amet"
+            },
+            startExpanded: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data: () => ({
+            expanded: false
+        }),
+        methods: {
+            toggle: function () {
+                this.expanded = !this.expanded;
+            }
+        },
+        mounted: function () {
+            this.expanded = this.startExpanded;
+        }
     }
 </script>
 
